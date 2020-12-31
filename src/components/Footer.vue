2ef32488b05202a19b7d11bf55e7ca52408dc3a8
lavender78 <lindawu8134@gmail.com>
2020-12-31 20:28:30 +0800

    feat: add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,44 +1,165 @@
 <template>
-  <footer class="py-12 footer">
-    <div class="max-w-6xl px-5 mx-auto">
-      <div class="flex flex-col space-y-5 md:flex-row md:space-y-0">
-        <div class="w-full mb-5 md:mb-0 md:w-1/4">
-          <img src="../images/logo_nocolor.png" class="h-8 mx-auto" />
-        </div>
-        <div class="w-full md:w-1/4">
-          <h4 class="uppercase mb-3 font-medium text-lg">Services</h4>
-          <ul class="md:text-sm flex space-x-5 md:flex-col md:space-x-0">
-            <li><a href="">About US</a></li>
-            <li><a href="">QA</a></li>
-          </ul>
-        </div>
-        <div class="md:w-1/2 flex">
-          <div class="w-1/2">
-            <h4 class="uppercase mb-3 font-medium text-lg">Support</h4>
-            <ul class="text-sm">
-              <li><a href="">Telegram Chinese</a></li>
-              <li><a href="">Telegram English</a></li>
-            </ul>
+  <div class="footer" height="78px">
+    <div class="footerContent">
+      <a href="/" class='logo'>
+        <img src="../images/logo.png" alt="pebbles" height='30'>
+      </a>
+      <div class='contractIcons'>
+        <a class='iconContent' href="ttps://www.chainnews.com/u/895866208677.htm" target="_blank">
+          <div class='chainNews'></div>
+        </a>
+        <a class='iconContent' href="https://xdefilab.medium.com/" target="_blank">
+          <font-awesome-icon :icon="['fab', 'medium-m']" size="lg"/>
+        </a>
+        <a class='iconContent' href="https://twitter.com/xdefilab" target="_blank">
+          <font-awesome-icon :icon="['fab', 'twitter']" size="lg"/>
+        </a>
+        <a class='iconContent' href="https://github.com/xdefilab" target="_blank">
+          <font-awesome-icon :icon="['fab', 'github']" size="lg"/>
+        </a>
+        <a class='iconContent' href="https://discord.gg/SuXhDHbAN3" target="_blank">
+          <font-awesome-icon :icon="['fab', 'discord']" size="lg"/>
+        </a>
+        <a class='iconContent' href="https://t.me/xdeficn" target="_blank">
+          <font-awesome-icon :icon="['fab', 'telegram-plane']" size="lg"/>
+          <span class='lang'>中</span>
+        </a>
+        <a class='iconContent' href="https://t.me/xdefi" target="_blank">
+          <font-awesome-icon :icon="['fab', 'telegram-plane']" size="lg"/>
+          <span class='lang'>EN</span>
+        </a>
+        <div class='weixin'>
+          <div class='iconContent' slot="reference" @click="showWeixin">
+            <font-awesome-icon :icon="['fab', 'weixin']" size="lg"/>
           </div>
-          <div class="w-1/2">
-            <h4 class="uppercase mb-3 font-medium text-lg">Contact</h4>
-            <ul class="text-sm">
-              <li><a href="">Email: info@xdefi.com</a></li>
-              <li><a href="">Wechat: xdefilab</a></li>
-              <li><a href="">Github: https://github.com/xdefilab</a></li>
-            </ul>
+          <div :class="['popover', weixin ? 'show' : 'hide']">
+            <img src='../images/wechat.png' alt='' width='150'/>
           </div>
         </div>
-      </div>
-      <div class="text-center mt-10 text-sm">
-        Copyright 2020 XDeFiLab. All Rights Reserved.
       </div>
     </div>
-  </footer>
+  </div>
 </template>
+<script>
+export default {
+  name: 'Footer',
+  data(){
+    return {
+      weixin: false
+    }
+  },
+  methods: {
+    showWeixin(){
+      this.weixin = !this.weixin;
+    }
+  }
+}
+</script>
+
 
 <style scoped>
-.footer {
-  color: rgb(76 77 125);
-}
+  .weixin{
+    position: relative;
+  }
+  .popover{
+    position: absolute;
+    bottom: 50px;
+    left: 10px;
+    background: #000;
+    width: 200px;
+    padding: 20px;
+    text-align:center;
+    
+  }
+  .popover.show{
+    display: block;
+  }
+  .popover.hide{
+    display: none;
+  }
+  .popover img{
+    display: inline;
+  }
+
+  .logo img{
+    height: 30px;
+  }
+  .footer {
+    background: #161638;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+  .footerContent {
+    padding: 10px 0;
+    width: 100%;
+    text-align: center;
+    display: flex !important;
+    justify-content: space-around;
+    align-items: center !important;
+
+  }
+  .footerContent  p {
+      color: #999;
+      margin: 0 2rem;
+      display: flex;
+      align-items: center;
+    }
+
+  .account {
+    display: none;
+  }
+
+  .contractIcons {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .iconContent {
+    width: 40px;
+    height: 40px;
+    margin: 0 8px;
+    background: rgba(83, 84, 132, 0.6);
+    border-radius: 3px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #000;
+    text-align: center;
+    position: relative;
+
+  }
+  .iconContent  .chainNews {
+      width: 35px;
+      height: 18px;
+      background: url('../images/chainNews.svg') 105% center no-repeat;
+      background-size: cover;
+      overflow: hidden;
+    }
+
+  .iconContent  .lang {
+      position: absolute;
+      bottom: 5px;
+      left: 5px;
+      font-size: 6px !important;
+      transform: scale(0.6);
+    }
+  @media (max-width: 768px) {
+    .footer {
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      z-index: 3001;
+
+    }
+    .footer .footerContent {
+      display: none !important;
+    }
+
+    .footer .account {
+      display: block;
+    }
+  }
 </style>
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,19 @@ import './css/tailwind.css'
 import './css/main.css'
 import { createApp, h } from 'vue'
 import App from './components/App.vue'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faDiscord, faTelegramPlane, faGithub, faTwitter, faMediumM, faWeixin } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
+config.autoAddCss = false
+
+library.add(faDiscord, faTelegramPlane, faGithub, faTwitter, faMediumM, faWeixin)
 
 const app = createApp({
   render: () => h(App),
 })
 
-app.mount('#root')
+app.use(ElementPlus).component('font-awesome-icon', FontAwesomeIcon).mount('#root')
